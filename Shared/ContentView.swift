0c319cbf5ce2eb9c7e9899bc5e8557bb5823e478
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-03-24 09:38:18 +0100

    Project Setup

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Gregor Hermanowski on 24. March.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/EventsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventsWidgetApp.swift
+//  Shared
+//
+//  Created by Gregor Hermanowski on 24. March.
+//
+
+import SwiftUI
+
+@main
+struct EventsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
