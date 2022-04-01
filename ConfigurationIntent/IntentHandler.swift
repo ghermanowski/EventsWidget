30fe8defff8e8ecc10dc2efc557aa2c8e66e5440
Gregor Hermanowski <57409167+ghermanowski@users.noreply.github.com>
2022-04-01 13:13:00 +0200

    Removed default implementation for IntentHandler.handler(for)

--- ConfigurationIntent/IntentHandler.swift
+++ ConfigurationIntent/IntentHandler.swift
@@ -28,11 +28,4 @@ class IntentHandler: INExtension, ConfigurationIntentHandling {
 		EventStore.shared.calendars()
 			.map { EWCalendar(identifier: $0.calendarIdentifier, display: $0.title) }
 	}
-	
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation.  If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent.
-        
-        return self
-    }
 }
